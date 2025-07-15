87aff971b482343a95b619bd4249a9f8cea9cb70
algosbiz <contact@algoseabiz.com>
2025-07-15 09:29:00 +0800

    update

--- index.js
+++ index.js
@@ -6,21 +6,15 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 const DOMAINS_MAP = {
-//   id: "https://penidadivecenter.com",
-//   tw: "https://penidadivecenter.tw",
-  fr: "https://penidadivecenter.fr",
+  es: "https://penidadivecenter.es",
 };
 
 const PROXIES = {
-  tw: process.env.BRD_PROXY_TW,
-  id: process.env.BRD_PROXY_ID,
-  fr: process.env.BRD_PROXY_FR,
+  es: process.env.BRD_PROXY_ES,
 };
 
 const USER_AGENTS = {
-  tw: "PenidaDiveCenter-CacheWarmer-TW/1.0",
-  id: "PenidaDiveCenter-CacheWarmer-ID/1.0",
-  fr: "PenidaDiveCenter-CacheWarmer-FR/1.0",
+  es: "PenidaDiveCenter-CacheWarmer-ES/1.0",
 };
 
 const CLOUDFLARE_ZONE_ID = process.env.CLOUDFLARE_ZONE_ID;
